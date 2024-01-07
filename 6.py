489553135e7d6b63c22b634735f8d478cc2dcf66
copy-cell <154884415+copy-cell@users.noreply.github.com>
2024-01-07 12:00:40 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,86 +1,35 @@
-import csv
-import random
-import math
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+data = pd.read_csv('6.csv')
 
-def mean(numbers):
-    return sum(numbers) / float(len(numbers))
+print("The first 5 Values of data are:\n", data.head())
 
+X = data.iloc[:, :-1]
+print("\nThe First 5 values of the train data are\n", X.head())
 
-def stdev(numbers):
-    avg = mean(numbers)
-    variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
-    return math.sqrt(variance)
+y = data.iloc[:, -1]
+print("\nThe First 5 values of train output are\n", y.head())
 
+categorical_columns = ['Outlook', 'Temperature', 'Humidity', 'Windy']
 
-def split_dataset(dataset, split_ratio=0.67):
-    train_size = int(len(dataset) * split_ratio)
-    train_set = random.sample(dataset, train_size)
-    test_set = [data for data in dataset if data not in train_set]
-    return train_set, test_set
+X = pd.get_dummies(X, columns=categorical_columns)
 
+print("\nNow the Train data is\n", X.head())
 
-def separate_by_class(dataset):
-    separated = {}
-    for vector in dataset:
-        class_value = vector[-1]
-        if class_value not in separated:
-            separated[class_value] = []
-        separated[class_value].append(vector)
-    return separated
+le_PlayTennis = LabelEncoder()
+y = le_PlayTennis.fit_transform(y)
 
+print("\nNow the Train output is\n", y)
 
-def summarize_dataset(dataset):
-    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-    del summaries[-1]
-    return summaries
+random_seed = 42
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=random_seed)
 
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
-def summarize_by_class(dataset):
-    separated = separate_by_class(dataset)
-    summaries = {}
-    for class_value, instances in separated.items():
-        summaries[class_value] = summarize_dataset(instances)
-    return summaries
-
-
-def calculate_probability(x, mean, stdev):
-    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
-    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
-
-
-def calculate_class_probabilities(summaries, input_vector):
-    probabilities = {}
-    for class_value, class_summaries in summaries.items():
-        probabilities[class_value] = 1
-        for i in range(len(class_summaries)):
-            mean, stdev = class_summaries[i]
-            x = input_vector[i]
-            probabilities[class_value] *= calculate_probability(x, mean, stdev)
-    return probabilities
-
-
-def predict(summaries, input_vector):
-    probabilities = calculate_class_probabilities(summaries, input_vector)
-    best_label = max(probabilities, key=probabilities.get)
-    return best_label
-
-
-def get_predictions(summaries, test_set):
-    predictions = [predict(summaries, data) for data in test_set]
-    return predictions
-
-
-def get_accuracy(test_set, predictions):
-    correct = sum(1 for i in range(len(test_set)) if test_set[i][-1] == predictions[i])
-    return (correct / float(len(test_set))) * 100.0
-
-
-with open('6.csv') as f:
-    data = [list(map(float, row)) for row in csv.reader(f)]
-    training_set, test_set = split_dataset(data)
-    print(f'Split {len(data)} rows into train={len(training_set)} and test={len(test_set)} rows')
-    summaries = summarize_by_class(training_set)
-    predictions = get_predictions(summaries, test_set)
-    accuracy = get_accuracy(test_set, predictions)
-    print(f'Accuracy of the classifier is: {accuracy}%')
+accuracy = accuracy_score(classifier.predict(X_test), y_test)
+print("Accuracy is:", accuracy)
